80c8d84a3e5618f94ea449ac600f6a76d45e9724
Bernard Baah <benbaah104@gmail.com>
2025-02-03 18:55:39 +0000

    update

--- cryptography/Blockchain-data-structure/Block.js
+++ cryptography/Blockchain-data-structure/Block.js
@@ -1,17 +1,22 @@
-const SHA256 = require("crypto-js/sha256");
+const SHA256 = require('crypto-js/sha256');
 
 class Block {
-	constructor(data, previousHash = "") {
-		this.data = data;
-		this.previousHash = previousHash;
-	}
-	toHash() {
-		const blockData = {
-			data: this.data,
-			previousHash: this.previousHash,
-		};
-		return SHA256(JSON.stringify(blockData)).toString();
-	}
+    constructor(data, previousHash = ""){
+        this.timestamp = Date.now();
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = SHA256(
+            this.timestamp +
+            JSON.stringify(this.data) +
+            this.previousHash)
+    }
+    toHash() {
+        const blockData = {
+            data: this.data,
+            previousHash: this.previousHash
+        }
+        return SHA256(JSON.stringify(blockData));
+    }
 }
 
 module.exports = Block;
--- cryptography/Blockchain-data-structure/Blockchain.js
+++ cryptography/Blockchain-data-structure/Blockchain.js
@@ -6,18 +6,24 @@ const Block = require("./Block");
 // Use this previousHash property in the calculation of the block's hash.
 
 class Blockchain {
-	constructor() {
-		const genesisBlock = new Block("Genesis block");
-		this.chain = [genesisBlock];
-	}
-	addBlock(block) {
-		// Get the hash of the last block to use as previousHash
-		const previousHash = this.chain[this.chain.length - 1].toHash();
+    constructor() {
+        const genesisBlock = new Block("Genesis block");
+        this.chain = [genesisBlock];
+    }
 
-		// Create new block with data and previousHash
-		const newBlock = new Block(block, previousHash);
-		this.chain.push(newBlock);
-	}
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(data) {
+        // Get the hash of the previous block
+        const previousHash = this.getLatestBlock().hash;
+
+        // Create a new block with the previous block's hash
+        const newBlock = new Block(data, previousHash);
+
+        // Add the new block to the chain
+        this.chain.push(newBlock);
+    }
 }
 
-module.exports = Blockchain;
