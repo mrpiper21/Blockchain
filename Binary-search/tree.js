060e24f1c3f6fa8b7aa311e8b7dd4126297fd0a9
Bernard <benbaah104@gmail.com>
2025-02-11 22:00:39 +0000

    Search root implementation

--- Binary-search/tree.js
+++ Binary-search/tree.js
@@ -1,29 +1,45 @@
 class Tree {
-    constructor() {
-        this.root = null;
-    }
-    addNode(newNode){
-        if(!this.root){
-            this.root = newNode;
-        }
-        this._addNodeRecursive(this.root, newNode)
-    }
-    _addNodeRecursive(currentNode, newNode) {
-        if (newNode.data < currentNode.data) {
-            if (!currentNode.left) {
-                currentNode.left = newNode;
-            } else {
-                this._addNodeRecursive(currentNode.left, newNode);
-            }
-        }
-        else if (newNode.data > currentNode.data) {
-            if (!currentNode.right) {
-                currentNode.right = newNode;
-            } else {
-                this._addNodeRecursive(currentNode.right, newNode);
-            }
-        }
-    }
+	constructor() {
+		this.root = null;
+	}
+	addNode(newNode) {
+		if (!this.root) {
+			this.root = newNode;
+		}
+		this._addNodeRecursive(this.root, newNode);
+	}
+	_addNodeRecursive(currentNode, newNode) {
+		if (newNode.data < currentNode.data) {
+			if (!currentNode.left) {
+				currentNode.left = newNode;
+			} else {
+				this._addNodeRecursive(currentNode.left, newNode);
+			}
+		} else if (newNode.data > currentNode.data) {
+			if (!currentNode.right) {
+				currentNode.right = newNode;
+			} else {
+				this._addNodeRecursive(currentNode.right, newNode);
+			}
+		}
+	}
+	hasNode(data) {
+		return this.searchRoot(this.root, data);
+	}
+	searchRoot(root, data) {
+		if (!root) {
+			return false;
+		}
+		if (root.data === data) {
+			return true;
+		}
+		if (root.data > data) {
+			return this.searchRoot(root.left, data);
+		}
+		if (root.data < data) {
+			return this.searchRoot(root.right, data);
+		}
+	}
 }
 
 module.exports = Tree;
